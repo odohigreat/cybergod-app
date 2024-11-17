44bf8d270d1e169bc58ec2a7ecb79077ce047a9c
Odohi Great <odohigreat@gmail.com>
2024-11-17 00:19:54 -0700

    build fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   plugins: [],
-
 
   darkMode: 'class',
 }
-
